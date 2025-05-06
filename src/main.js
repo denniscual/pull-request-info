204a34d526808296ae4299d63cfdba376f684b77
Dennis Martin Cual <denniscual0716@gmail.com>
2025-05-06 18:52:09 +0800

    Revert "Github Action for tracking the Pull Request based on Commit SHA (#3)"
    This reverts commit caa93efb31394f433164f8973af2d0e28415f345.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { Octokit } from '@octokit/core'
+import { wait } from './wait.js'
 
 /**
  * The main function for the action.
@@ -8,47 +8,20 @@ import { Octokit } from '@octokit/core'
  */
 export async function run() {
   try {
-    const commitSha = core.getInput('commit_sha')
-    const githubToken = core.getInput('github_token')
-    const owner = core.getInput('owner')
-    const repo = core.getInput('repo')
+    const ms = core.getInput('milliseconds')
 
-    core.debug(`Commit SHA: ${commitSha}`)
-    core.debug(`Owner: ${owner}`)
-    core.debug(`Repo: ${repo}`)
+    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+    core.debug(`Waiting ${ms} milliseconds ...`)
 
-    // Initialize Octokit client with the provided token
-    const octokit = new Octokit({
-      auth: githubToken
-    })
+    // Log the current timestamp, wait, then log the new timestamp
+    core.debug(new Date().toTimeString())
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
 
-    // Make request to get pull requests associated with the commit
-    core.debug(`Fetching pull requests for commit ${commitSha}...`)
-    const response = await octokit.request(
-      'GET /repos/{owner}/{repo}/commits/{commit_sha}/pulls',
-      {
-        owner,
-        repo,
-        commit_sha: commitSha,
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      }
-    )
-
-    core.debug(
-      `Found ${response.data.length} pull requests associated with the commit`
-    )
-
-    // Set the output as specified in action.yml
-    core.setOutput('pull_requests', JSON.stringify(response.data))
+    // Set outputs for other workflow steps to use
+    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     // Fail the workflow run if an error occurs
-    if (error instanceof Error) {
-      core.error(error)
-    }
-
-    // Avoid failing the workflow run if there is an error occurring in the action. Instead, set output to empty array
-    core.setOutput('pull_requests', JSON.stringify([]))
+    if (error instanceof Error) core.setFailed(error.message)
   }
 }
